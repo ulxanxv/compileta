5ca20c890225bd2230d1d9a1f8b312a51674b069
artemulyanov <artem.ulyanov@bsc-ideas.com>
2021-07-05 11:26:51 +0300

    Revert "test1"
    This reverts commit a73cd391faff75d6f5db079690e0a5a3e7c861a0.

--- src/main/java/ru/dexterity/compileta/api/CompileController.java
+++ src/main/java/ru/dexterity/compileta/api/CompileController.java
@@ -1,7 +1,6 @@
 package ru.dexterity.compileta.api;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +13,6 @@ import ru.dexterity.compileta.exceptions.CompilationErrorException;
 
 import java.util.Map;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class CompileController {
